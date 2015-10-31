ed45abc3628fde47584630f283e0f14930d038b5
Kenneth Liang <kennethliang0@gmail.com>
2015-10-31 16:07:12 -0400

    Evolution Strategy done

--- PacManAI/src/pacman/controllers/examples/StarterPacMan.java
+++ PacManAI/src/pacman/controllers/examples/StarterPacMan.java
@@ -1,8 +1,10 @@
 package pacman.controllers.examples;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Random;
 
 import pacman.controllers.Controller;
@@ -51,7 +53,99 @@ public class StarterPacMan extends Controller<MOVE> {
 		//return getMoveIterativeDeepening(game, ghostMoves, DEPTH);
 		//return getMoveHillClimber(ghostMoves, tree);
 		//return getMoveAlphabeta(game, ghostMoves, DEPTH);
-		return getMoveSimulatedAnnealing(ghostMoves, tree);
+		//return getMoveSimulatedAnnealing(ghostMoves, tree);
+		return getMoveEvolutionStrategy(game, ghostMoves, DEPTH);
+	}
+	
+	/*
+	 * Depth is the number of generations.
+	 * Each generation will have the best fitness (best heuristic) game state mutate by advancing
+	 * the state with a MOVE. After depth number of generations, return MOVE leading to best fitness game state.
+	 */
+	public MOVE getMoveEvolutionStrategy(Game game, EnumMap<GHOST, MOVE> ghostMoves, int depth) {
+		long start = System.currentTimeMillis();
+		PriorityQueue<Node> population = new PriorityQueue<Node>(25, new NodeComparator());
+		
+		// initializing population; counts as one depth
+		for (int i = 0; i < 4; i++) {
+			Node childNode = new Node();
+			Game gameState = game.copy();
+			switch(i) {
+			case 0:
+				gameState.advanceGame(MOVE.LEFT, ghostMoves);
+				childNode.setGameState(gameState);
+				childNode.setMove(MOVE.LEFT); // sets the move to this node
+				break;
+			case 1:
+				gameState.advanceGame(MOVE.RIGHT, ghostMoves);
+				childNode.setGameState(gameState);
+				childNode.setMove(MOVE.RIGHT); // sets the move to this node
+				break;
+			case 2:
+				gameState.advanceGame(MOVE.UP, ghostMoves);
+				childNode.setGameState(gameState);
+				childNode.setMove(MOVE.UP); // sets the move to this node
+				break;
+			case 3:
+				gameState.advanceGame(MOVE.DOWN, ghostMoves);
+				childNode.setGameState(gameState);
+				childNode.setMove(MOVE.DOWN); // sets the move to this node
+				break;
+			}
+			population.add(childNode);
+		}
+		
+		// mutate for depth - 1 times
+		for (int i = 0; i < depth - 1; i++) {
+			Node bestNode = population.peek();
+			for (int j = 0; j < 4; j++) {
+				Node childNode = new Node();
+				Game gameState = bestNode.getGameState().copy();
+				switch(j) {
+				case 0:
+					gameState.advanceGame(MOVE.LEFT, ghostMoves);
+					childNode.setGameState(gameState);
+					childNode.setMove(bestNode.getMove());
+					break;
+				case 1:
+					gameState.advanceGame(MOVE.RIGHT, ghostMoves);
+					childNode.setGameState(gameState);
+					childNode.setMove(bestNode.getMove());
+					break;
+				case 2:
+					gameState.advanceGame(MOVE.UP, ghostMoves);
+					childNode.setGameState(gameState);
+					childNode.setMove(bestNode.getMove());
+					break;
+				case 3:
+					gameState.advanceGame(MOVE.DOWN, ghostMoves);
+					childNode.setGameState(gameState);
+					childNode.setMove(bestNode.getMove());
+					break;
+				}
+				population.add(childNode);
+			}
+		}
+		
+		if (LOG_TIME) System.out.println(System.currentTimeMillis() - start);
+		return population.peek().getMove();
+	}
+	
+	private class NodeComparator implements Comparator<Node> {
+		// higher heuristic values get higher priority
+		@Override
+		public int compare(Node firstNode, Node secondNode) {
+			int firstHeuristic = evaluateGameState(firstNode.getGameState());
+			int secondHeuristic = evaluateGameState(secondNode.getGameState());
+			
+			if (firstHeuristic < secondHeuristic) {
+				return 1;
+			} else if (firstHeuristic == secondHeuristic) {
+				return 0;
+			} else {
+				return -1;
+			}
+		}
 	}
 	
 	int getBestHeuristicAlphabeta(Game gameState, EnumMap<GHOST, MOVE> ghostMoves, boolean maximizingPlayer, int alpha, int beta, int depth) {
